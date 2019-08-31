9f1733ce106f9b723fbe9dcd47a0b60a072320d6
RamiTae <trmi422@gmail.com>
2019-08-31 23:26:45 +0900

    js to html test

--- Html/main.js
+++ Html/main.js
@@ -1,2 +1,6 @@
 // main.js
 console.log(document.getElementById('greeting'));
+const getGreeting = document.querySelector('#greeting');
+console.log(getGreeting);
+getGreeting.innerHTML = 'Hi greeting';
+getGreeting.innerHTML = '<div>In div</div>'
